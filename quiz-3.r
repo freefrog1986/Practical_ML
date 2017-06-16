c5e2acc19e6b4240b5a25cab0f1421472a6c37eb
freefrog1986 <baihu1111@126.com>
2017-06-16 16:44:09 +0800

    init repo

--- quiz-3.r
+++ quiz-3.r
@@ -37,4 +37,55 @@ text(mod1$finalModel,use.n=TRUE, all=TRUE,cex=.8)
 # The bias is larger and the variance is smaller. Under leave one out cross validation K is equal to one.
 
 
+# Load the olive oil data using the commands:
+data(olive)
+olive$Area <- as.factor(olive$Area)
+olive = olive[,-1]
+mod2 <- train(Area~., method ="rpart", data=olive)
+newdata = as.data.frame(t(colMeans(olive)))
+predict(mod2,newdata = newdata)
+
+
+
+# Load the South Africa Heart Disease Data and create training and test sets with the following code:
+library(ElemStatLearn)
+data(SAheart)
+set.seed(8484)
+train = sample(1:dim(SAheart)[1],size=dim(SAheart)[1]/2,replace=F)
+trainSA = SAheart[train,]
+testSA = SAheart[-train,]
+# Then set the seed to 13234 and fit a logistic regression model (method="glm", be sure to specify family="binomial") with Coronary Heart Disease (chd) as the outcome and age at onset, current alcohol consumption, obesity levels, cumulative tabacco, type-A behavior, and low density lipoprotein cholesterol as predictors. Calculate the misclassification rate for your model using this function and a prediction on the "response" scale:
+set.seed(13234)
+mod3<- train(chd~age+alcohol+obesity+tobacco+typea+ldl,data=SAheart,method='glm', family="binomial")
+missClass = function(values,prediction){sum(((prediction > 0.5)*1) != values)/length(values)}
+
+
+# Load the vowel.train and vowel.test data sets:
+library(ElemStatLearn)
+data(vowel.train)
+data(vowel.test)
+# Set the variable y to be a factor variable in both the training and test set. Then set the seed to 33833. Fit a random forest predictor relating the factor variable y to the remaining variables. Read about variable importance in random forests here: http://www.stat.berkeley.edu/~breiman/RandomForests/cc_home.htm#ooberr The caret package uses by default the Gini importance.
+# 
+# Calculate the variable importance using the varImp function in the caret package. What is the order of variable importance?
+# 
+# [NOTE: Use randomForest() specifically, not caret, as there's been some issues reported with that approach. 11/6/2016]
+vowel.train$y<-as.factor(vowel.train$y)
+vowel.test$y<-as.factor(vowel.test$y)
+set.seed(33833)
+mod4<-train(y~.,data=vowel.train,method='rf',prox=TRUE)
+mod4<- randomForest(y ~ ., data=vowel.train, importance=0,proximity=TRUE)
+round(importance(mod4), 2)
+varImp(mod4)
+
+
+
+
+
+
+
+
+
+
+
+
 
